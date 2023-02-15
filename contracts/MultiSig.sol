f5a23b9e189a2aa3e7977ee662507e324ba26d9e
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-15 20:39:56 +0000

    Handle Constructor Mistakes TO revert the deployment transaction
    When developing a user friendly contract we should be
    validating user inputs for common sources of error.

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.4;
 
 contract MultiSig {
-    address[] public owners;
-    uint public transactionCount;
+    address[] public owners; // store wallet owner addresses
+    uint public transactionCount; //store the required amount of confirmations needed to execute a transaction
     uint public required;
 
     struct Transaction {
@@ -84,9 +84,13 @@ contract MultiSig {
     }
 
     constructor(address[] memory _owners, uint _confirmations) {
-        require(_owners.length > 0);
-        require(_confirmations > 0);
-        require(_confirmations <= _owners.length);
+        //revert the deployment transaction in the following situations:
+        //1. if the number of owners is 0
+        //2. if the number of confirmations is 0
+        //3. if the number of confirmations is greater than the number of owners
+        require(_owners.length > 0); //    // No owner addresses are sent.
+        require(_confirmations > 0); //Number of required confirmations is zero.
+        require(_confirmations <= _owners.length); // Number of required confirmations is more than the total number of owner addresses.
         owners = _owners;
         required = _confirmations;
     }
