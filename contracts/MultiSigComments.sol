9b4b8ffc86ca00a0c85c10cfe697dd3db684b60c
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-15 21:26:53 +0000

    Small Updates

--- contracts/MultiSigComments.sol
+++ contracts/MultiSigComments.sol
@@ -2,35 +2,42 @@
 pragma solidity ^0.8.4;
 
 contract MultiSig {
-    address[] public owners;
-    uint public transactionCount;
+    address[] public owners; // store wallet owner addresses
+    uint public transactionCount; //store the required amount of confirmations needed to execute a transaction
     uint public required;
 
+    //Define a Transaction struct that includes
     struct Transaction {
-        address payable destination;
-        uint value;
-        bool executed;
+        address destination; //destination of the transaction's value.
+        uint value; //value of the transaction in wei.
+        bool executed; // indicates if the transaction has been executed.
     }
 
+    //Define a mapping that maps transaction IDs to Transaction structs
     mapping(uint => Transaction) public transactions;
+
+    //maps the transaction id (uint) to an owner (address) to whether or not they have confirmed the transaction (bool).
     mapping(uint => mapping(address => bool)) public confirmations;
 
-    receive() external payable {}
-
+    //The transaction should only execute if it is confirmed. If not, revert the transaction.
     function executeTransaction(uint transactionId) public {
         require(isConfirmed(transactionId));
         Transaction storage _tx = transactions[transactionId];
         (bool success, ) = _tx.destination.call{value: _tx.value}("");
         require(success);
-        _tx.executed = true;
+        _tx.executed = true; //Once transferred, set boolean to true
     }
 
+    // payable receive function that allows our Multi-Sig wallet to accept funds
+    receive() external payable {}
+
     function isConfirmed(uint transactionId) public view returns (bool) {
+        //Return true if the transaction is confirmed and false if it is not.
         return getConfirmationsCount(transactionId) >= required;
     }
 
     function getConfirmationsCount(
-        uint transactionId
+        uint transactionId //representing the number of times the transaction with the given transactionId has been confirmed.
     ) public view returns (uint) {
         uint count;
         for (uint i = 0; i < owners.length; i++) {
@@ -41,6 +48,7 @@ contract MultiSig {
         return count;
     }
 
+    //Ensure that confirmTransaction can only be called by the owners stored in the constructor.
     function isOwner(address addr) private view returns (bool) {
         for (uint i = 0; i < owners.length; i++) {
             if (owners[i] == addr) {
@@ -50,11 +58,13 @@ contract MultiSig {
         return false;
     }
 
-    function submitTransaction(address payable dest, uint value) external {
+    //function that will allow a user to create a transaction and immediately confirm it.
+    function submitTransaction(address dest, uint value) external {
         uint id = addTransaction(dest, value);
         confirmTransaction(id);
     }
 
+    // This function should create a confirmation for the transaction from the msg.sender.
     function confirmTransaction(uint transactionId) public {
         require(isOwner(msg.sender));
         confirmations[transactionId][msg.sender] = true;
@@ -63,19 +73,24 @@ contract MultiSig {
         }
     }
 
+    //  function to put our transactions into that storage!
     function addTransaction(
-        address payable destination,
+        address destination,
         uint value
-    ) public returns (uint) {
+    ) internal returns (uint) {
         transactions[transactionCount] = Transaction(destination, value, false);
         transactionCount += 1;
         return transactionCount - 1;
     }
 
     constructor(address[] memory _owners, uint _confirmations) {
-        require(_owners.length > 0);
-        require(_confirmations > 0);
-        require(_confirmations <= _owners.length);
+        //revert the deployment transaction in the following situations:
+        //1. if the number of owners is 0
+        //2. if the number of confirmations is 0
+        //3. if the number of confirmations is greater than the number of owners
+        require(_owners.length > 0); //    // No owner addresses are sent.
+        require(_confirmations > 0); //Number of required confirmations is zero.
+        require(_confirmations <= _owners.length); // Number of required confirmations is more than the total number of owner addresses.
         owners = _owners;
         required = _confirmations;
     }
